72f7d0e431486f0599fe74304d8267d5b4afbcc2
zhangYue <zhangy5005@chinaunicom.cn>
2025-09-18 10:50:02 +0800

    Site updated: 2025-09-18 10:50:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/17/hello-world/","2025/09/18/我的第一篇博客/"];function toRandomPost(){
+var posts=["2025/09/18/我的第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
